eae1924bc300300fb85d647a0e5b94f74ad2a1d3
Sreekanth K <sreekanth100k@gmail.com>
2021-03-13 16:57:31 +0530

    Commented some unwanted code

--- AndroidStudioProjects/HomePageActivity/app/src/main/java/com/android/activities/DetailsActivity.kt
+++ AndroidStudioProjects/HomePageActivity/app/src/main/java/com/android/activities/DetailsActivity.kt
@@ -1,12 +1,17 @@
 package com.android.activities
+
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class DetailsActivity : AppCompatActivity() {
@@ -14,7 +19,7 @@ class DetailsActivity : AppCompatActivity() {
     var mPopulationTv:TextView? = null
     var mRegionTv:TextView? = null
     var mCapitalTv:TextView? = null
-    var mFlagIv:ImageView? = null
+//    var mFlagIv:ImageView? = null
 
     var mTitleTv:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,24 +31,35 @@ class DetailsActivity : AppCompatActivity() {
         var countryName: String?    = intent.getStringExtra("countryName")
         var capital: String?        = intent.getStringExtra("capital")
         var region: String?         = intent.getStringExtra("region")
-        var population:Int?      = intent.getIntExtra("population",0)
-        var flag:String?            = intent.getStringExtra("flag")
+        var population:Int?      = intent.getIntExtra("population", 0)
+//        var flag:String?            = intent.getStringExtra("flag")
 
         mTitleTv         =   findViewById(R.id.id_details_title_tv)
         mPopulationTv    =  findViewById(R.id.id_population_value_tv)
         mRegionTv        =  findViewById(R.id.id_region_value_tv)
         mCapitalTv       =  findViewById(R.id.id_capital_value_tv)
-        mFlagIv                 =  findViewById(R.id.id_flag_iv)
+//        mFlagIv                 =  findViewById(R.id.id_flag_iv)
         mTitleTv?.text   =   "Details :" + countryName
 
         mPopulationTv?.text = population.toString()
         mRegionTv?.text = region
         mCapitalTv?.text = capital
 
-        Glide.with(this).load(flag)
-            .placeholder(R.drawable.ic_launcher_background)
-            .error(R.drawable.ic_launcher_background)
-            .into(mFlagIv);
+        /*Glide not working
+         */
+//            try {
+//                flag = flag?.replace("https","http",false)
+//                Glide.with(this)
+//                    .load(flag)
+//                .into(mFlagIv!!);
+//            } catch (e: IOException) {
+//            }
+//
+
+
+
+
+
 
 
     }
@@ -51,5 +67,19 @@ class DetailsActivity : AppCompatActivity() {
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+
+    fun getBitmapFromURL(src: String?): Bitmap? {
+        return try {
+            val url = URL(src)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.setDoInput(true)
+            connection.connect()
+            val input: InputStream = connection.getInputStream()
+            BitmapFactory.decodeStream(input)
+        } catch (e: IOException) {
+            // Log exception
+            null
+        }
     }
 }
